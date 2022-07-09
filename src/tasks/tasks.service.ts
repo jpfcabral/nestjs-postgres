cb453067406f8d3f80ade2109ce16da3adf75d9e
jpfcabral <jpfcabral@gmail.com>
2022-07-09 17:13:00 -0300

    Search tasks with filters

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -13,25 +13,23 @@ export class TasksService {
         @InjectRepository(Task)
         private tasksRepository: Repository<Task>
     ){}
-    // getAllTasks(): Task[] {
-    //     return this.tasks;
-    // }
 
-    // getTaskWithFilters(filterDto: GetTasksFilterDto): Task[] {
-    //     const { status, search } = filterDto;
-    //     let tasks = this.getAllTasks();
+    async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+        const { status, search } = filterDto;
 
-    //     if (status) {
-    //         tasks = tasks.filter(task => task.status === status);
-    //     }
+        const query = this.tasksRepository.createQueryBuilder('task');
 
-    //     if (search) {
-    //         tasks = tasks.filter(task => task.title.includes(search) || task.description.includes(search));
-    //     }
+        if (status) {
+            query.andWhere('task.status = :status', { status });
+        }
 
-    //     return tasks;
-    // }
-    
+        if (search) {
+            query.andWhere('(task.title LIKE :search OR task.description LIKE :search)', { search: `%${search}%` });
+        }
+        
+        const tasks = await query.getMany();
+        return tasks;
+    }
     async getTaskById(id: string): Promise<Task> {
         const found = await this.tasksRepository.findOne({where: {id: id}});
         if (!found) {
